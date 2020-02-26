a5d428414f23e69d2e100d4b563c1e3f56a424f0
Zach Barbre <zach.barbre@gmail.com>
2020-02-26 09:13:44 -0500

    finish exercise 2

--- exercise2.js
+++ exercise2.js
@@ -39,6 +39,53 @@ class Hand {
         this.hand.push(card);
     }
     getPoints(){
-        /// finish this
+        return this.hand.reduce((acc, card) => {
+            return acc + card.point;
+        }, 0);
     }
 }
+
+class Deck {
+    constructor(){
+        this.deck = [];
+        this.suits = ['spades', 'clubs', 'hearts', 'diamonds'];
+        this.faces = ['Jack', 'Queen', 'King'];
+        this.suits.forEach(suit => {
+            for (let i = 1; i <= 10; i++){
+                this.deck.push(new Card(i, suit));
+            }
+            this.faces.forEach(face => {
+                this.deck.push(new Card(face, suit));
+            });
+        });
+    }
+
+    shuffle(){
+        let shuffledDeck = [];
+        while (shuffledDeck.length < this.deck.length){
+            const index = Math.floor(Math.random()*52);
+            if (shuffledDeck.includes(this.deck[index]) === false){
+                shuffledDeck.push(this.deck[index]);
+            }
+        }
+        this.deck = shuffledDeck;
+    }
+
+    draw(){
+        return this.deck.pop();
+    }
+    
+    numCardsLeft(){
+        return this.deck.length;
+    }
+}
+
+const myHand = new Hand()
+myHand.addCard(new Card(5, 'diamonds'))
+myHand.addCard(new Card(13, 'spades'))
+console.log(myHand.getPoints());
+console.log(myHand.hand);
+
+// const myDeck = new Deck();
+// myDeck.shuffle();
+// console.log(myDeck.deck);
